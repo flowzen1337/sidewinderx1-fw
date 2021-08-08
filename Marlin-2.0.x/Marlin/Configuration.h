0cebb3c0cb54c8ba716be148da07c038bf3d9eea
flowzen1337 <floklinger85@aol.com>
2021-08-08 20:06:41 +0200

    Changed BL Touch Offset and added fully ready to use firmware

--- Marlin-2.0.x/Marlin/Configuration.h
+++ Marlin-2.0.x/Marlin/Configuration.h
@@ -1132,7 +1132,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 33, -22, -3.60 }              // Flowzen1337
+#define NOZZLE_TO_PROBE_OFFSET { 33, -22, -3.80 }              // Flowzen1337
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
